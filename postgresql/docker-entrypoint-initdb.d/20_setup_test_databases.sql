b101bbdf4e396619ec79f670c8508c8cd4d0cc75
Stefanie Taepke <stefanie.taepke@liip.ch>
2023-07-27 19:00:27 +0200

    fix: use correct values for database setup

--- postgresql/docker-entrypoint-initdb.d/20_setup_test_databases.sql
+++ postgresql/docker-entrypoint-initdb.d/20_setup_test_databases.sql
@@ -1,2 +1,2 @@
-CREATE DATABASE ckan_test OWNER ckan ENCODING 'utf-8';
-CREATE DATABASE datastore_test OWNER ckan ENCODING 'utf-8';
+CREATE DATABASE ckan_test OWNER ckandbuser ENCODING 'utf-8';
+CREATE DATABASE datastore_test OWNER ckandbuser ENCODING 'utf-8';
